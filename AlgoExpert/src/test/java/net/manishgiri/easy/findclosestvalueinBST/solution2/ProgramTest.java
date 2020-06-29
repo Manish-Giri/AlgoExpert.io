07f177ce4de434a2c61e20fe4550ccfeddd46de5
manish-giri <manish.giri.me@gmail.com>
2020-06-28 23:39:38 -0400

    Add test for recursive O(n) solution for Find Closest Value in BST

---
+++ AlgoExpert/src/test/java/net/manishgiri/easy/findclosestvalueinBST/solution2/ProgramTest.java
@@ -0,0 +1,25 @@
+package net.manishgiri.easy.findclosestvalueinBST.solution2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void findClosestValueInBst() {
+        Program.BST root = new Program.BST(10);
+        root.left = new Program.BST(5);
+        root.left.left = new Program.BST(2);
+        root.left.left.left = new Program.BST(1);
+        root.left.right = new Program.BST(5);
+        root.right = new Program.BST(15);
+        root.right.left = new Program.BST(13);
+        root.right.left.right = new Program.BST(14);
+        root.right.right = new Program.BST(22);
+
+        int expected = 13;
+        int actual = Program.findClosestValueInBst(root, 12);
+        assertEquals(expected, actual);
+    }
+}
