3e6fd6cb901b3ecba7a42c1628be3f23dc23553e
manish-giri <manish.giri.me@gmail.com>
2020-09-07 17:48:42 -0400

    Write test for Bubble Sort (Optimized version)

---
+++ AlgoExpert/src/test/java/net/manishgiri/easy/bubblesort/optimized/ProgramTest.java
@@ -0,0 +1,16 @@
+package net.manishgiri.easy.bubblesort.optimized;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void bubbleSort() {
+        int[] arr = {8, 5, 2, 9, 5, 6, 3};
+        int[] result = Program.bubbleSort(arr);
+        assertArrayEquals(result, new int[]{2, 3, 5, 5, 6, 8, 9});
+    }
+}
