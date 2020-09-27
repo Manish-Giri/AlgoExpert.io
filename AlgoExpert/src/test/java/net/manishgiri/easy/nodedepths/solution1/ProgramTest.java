a9ceb45da9eb88e33ba7b815339398aedbd82ab6
manish-giri <manish.giri.me@gmail.com>
2020-09-26 22:11:57 -0400

    Add test for first recursive solution for Node Depths (Easy)

---
+++ AlgoExpert/src/test/java/net/manishgiri/easy/nodedepths/solution1/ProgramTest.java
@@ -0,0 +1,23 @@
+package net.manishgiri.easy.nodedepths.solution1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void nodeDepths() {
+        Program.BinaryTree root = new Program.BinaryTree(1);
+        root.left = new Program.BinaryTree(2);
+        root.left.left = new Program.BinaryTree(4);
+        root.left.left.left = new Program.BinaryTree(8);
+        root.left.left.right = new Program.BinaryTree(9);
+        root.left.right = new Program.BinaryTree(5);
+        root.right = new Program.BinaryTree(3);
+        root.right.left = new Program.BinaryTree(6);
+        root.right.right = new Program.BinaryTree(7);
+        int actual = Program.nodeDepths(root);
+        assertEquals(16, actual);
+    }
+}
