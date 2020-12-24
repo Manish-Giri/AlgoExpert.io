c749a978f7e78b59b6b330793681845441264b7e
Manish-Giri <manish.giri.me@gmail.com>
2020-12-23 22:57:41 -0500

    Create class for Permutations optimized

---
+++ AlgoExpert/src/main/java/net/manishgiri/categories/recursion/permutations/optimized/Program.java
@@ -0,0 +1,5 @@
+package net.manishgiri.categories.recursion.permutations.optimized;
+
+// uses swapping to create permutations within the same array, thereby avoiding extra arrays
+class Program {
+}
