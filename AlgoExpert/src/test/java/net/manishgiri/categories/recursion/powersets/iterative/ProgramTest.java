37876cf40344c7ccf8c3432b7949f45a56250929
Manish-Giri <manish.giri.me@gmail.com>
2020-12-26 00:15:09 -0500

    Test for Iterative solution for Powersets (Recursion)

---
+++ AlgoExpert/src/test/java/net/manishgiri/categories/recursion/powersets/iterative/ProgramTest.java
@@ -0,0 +1,49 @@
+package net.manishgiri.categories.recursion.powersets.iterative;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void powerset() {
+        List<List<Integer>> output = Program.powerset(new ArrayList<Integer>(Arrays.asList(1, 2, 3)));
+        assertEquals(output.size(), 8);
+        assertTrue(contains(output, new int[] {}));
+        assertTrue(contains(output, new int[] {1}));
+        assertTrue(contains(output, new int[] {2}));
+        assertTrue(contains(output, new int[] {1, 2}));
+        assertTrue(contains(output, new int[] {3}));
+        assertTrue(contains(output, new int[] {1, 3}));
+        assertTrue(contains(output, new int[] {2, 3}));
+        assertTrue(contains(output, new int[] {1, 2, 3}));
+    }
+
+    private boolean contains(List<List<Integer>> arr1, int[] arr2) {
+        for (List<Integer> subArr : arr1) {
+            Collections.sort(subArr);
+            if (compare(subArr, arr2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean compare(List<Integer> arr1, int[] arr2) {
+        if (arr1.size() != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.size(); i++) {
+            if (arr1.get(i) != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
